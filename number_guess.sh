fe9e4aac54bd3f102ae2977964937366f00ac573
Qifays ibr <afim121@gmail.com>
2025-04-23 06:07:24 +0000

    fix: fixing bugs and error

--- number_guess.sh
+++ number_guess.sh
@@ -2,37 +2,42 @@
 
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
-echo "Enter your username: "
+echo "Enter your username:"
 read USERNAME
 
-USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME'" | xargs)
+USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME'")
 
 if [[ -z $USER_ID ]]; then
-  $PSQL "INSERT INTO users(username) VALUES('$USERNAME')"
+  INSERT_USER_RESULT=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME')")
   echo "Welcome, $USERNAME! It looks like this is your first time here."
-  USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME'" | xargs)
+  USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME'")
 else
-  GAMES_PLAYED=$($PSQL "SELECT COUNT(*) FROM games WHERE user_id=$USER_ID" | xargs)
-  BEST_GAME=$($PSQL "SELECT MIN(guesses) FROM games WHERE user_id=$USER_ID" | xargs)
+  GAMES_PLAYED=$($PSQL "SELECT COUNT(game_id) FROM games WHERE user_id=$USER_ID")
+  BEST_GAME=$($PSQL "SELECT MIN(guesses) FROM games WHERE user_id=$USER_ID")
   echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
 fi
 
-SECRET_NUMBER=$(( RANDOM % 1000 + 1))
+SECRET_NUMBER=$(( RANDOM % 1000 + 1 ))
+GUESS_COUNT=0
+
 echo "Guess the secret number between 1 and 1000:"
 
-NUMBER_OF_GUESSES=0
 while true; do
   read GUESS
-  ((NUMBER_OF_GUESSES++))
-  if ! [[ $GUESS =~ ^[0-9]+$ ]]; then
+  ((GUESS_COUNT++))
+  
+  if [[ ! $GUESS =~ ^[0-9]+$ ]]; then
     echo "That is not an integer, guess again:"
-  elif (( GUESS < SECRET_NUMBER )); then
+    continue
+  fi
+
+  if (( GUESS < SECRET_NUMBER )); then
     echo "It's higher than that, guess again:"
   elif (( GUESS > SECRET_NUMBER )); then
     echo "It's lower than that, guess again:"
   else
-    echo "You guessed it in $NUMBER_OF_GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
-    $PSQL "INSERT INTO games(user_id, guesses) VALUES($USER_ID, $NUMBER_OF_GUESSES)"
+    INSERT_GAME_RESULT=$($PSQL "INSERT INTO games(user_id, guesses) VALUES($USER_ID, $GUESS_COUNT)")
+    echo "You guessed it in $GUESS_COUNT tries. The secret number was $SECRET_NUMBER. Nice job!"
     break
   fi
 done
