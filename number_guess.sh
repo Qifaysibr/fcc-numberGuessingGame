b526e0ad5474ff4af7e2e8350521c94c5728de6e
Qifays ibr <afim121@gmail.com>
2025-04-23 04:32:36 +0000

    feat: add games logic

--- number_guess.sh
+++ number_guess.sh
@@ -17,3 +17,22 @@ else
   echo "Welcome, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses. "
 fi
 
+SECRET_NUMBER=$(( RANDOM % 1000 + 1))
+echo "Guess the secret number between 1 and 1000:"
+
+NUMBER_OF_GUESSES=0
+while true; do
+  read GUESS
+  ((NUMBER_OF_GUESSES++))
+  if ! [[ $GUESS =- ^[0-9]+$ ]]; then
+    echo "That is not an integer, guess again:"
+  elif (( GUESS < SECRET_NUMBER )); then
+    echo "It's higher than that, guess again:"
+  elif (( GUESS > SECRET_NUMBER )); then
+    echo "It's lower than that, guess again:"
+  else
+    echo "You guessed it in $NUMBER_OF_GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
+    $PSQL "INSERT INTO games(user_id, guesses) VALUES($USER_ID, $NUMBER_OF_GUESSES)"
+    break
+  fi
+done
