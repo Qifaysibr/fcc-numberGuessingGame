21128d01a44e9ffc94c68613847f02435cb20ab5
Qifays ibr <afim121@gmail.com>
2025-04-23 04:10:15 +0000

    feat: create username input field

--- number_guess.sh
+++ number_guess.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
+
+echo "Enter your username: "
+read USERNAME
+
+USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME'")
+
