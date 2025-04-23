1faa6168c8aa95a75352ddd9447d5862e459e97d
Qifays ibr <afim121@gmail.com>
2025-04-23 04:22:07 +0000

    feat: check username

--- number_guess.sh
+++ number_guess.sh
@@ -7,3 +7,13 @@ read USERNAME
 
 USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME'")
 
+if [[ -z $USER_ID ]]; then
+  echo "$(PSQL "INSERT INTO users(username) VALUES('$USERNAME')")"
+  echo "Welcome, $USERNAME! It looks like this is your first time here."
+  USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME'")
+else
+  GAMES_PLAYED=$($PSQL "SELECT COUNT(*) FROM games WHERE user_id=$USER_ID")
+  BEST_GAME=$($PSQL "SELECT MIN(guesses) FROM game WHERE user_id=$USER_ID")
+  echo "Welcome, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses. "
+fi
+
